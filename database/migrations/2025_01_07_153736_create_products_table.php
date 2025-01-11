595247bbe9cde330895ec6199e3aebf9a939be09
aipnurhayadi08 <aipnurhayadi08@gmail.com>
2025-01-11 21:29:56 +0700

    fix: perbaikan category product

--- database/migrations/2025_01_07_153736_create_products_table.php
+++ database/migrations/2025_01_07_153736_create_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('category');
+            $table->enum('category', ['snack_drink', 'pizza', 'pasta_rice']);
             $table->string('code');
             $table->string('name');
             $table->text('description');
